abbc60a6beb9b113de0715665cadc83323c4b35a
Cary Carter <ccarterdev@gmail.com>
2024-01-20 14:36:39 -0800

    functional tests

--- app/db.py
+++ app/db.py
@@ -1,6 +1,9 @@
 import os
 from . import models
 from . import config
+
+# this next line is required apparently for sqlite
+from sqlalchemy.dialects import sqlite
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
 
@@ -8,16 +11,17 @@ engine = create_engine(config.DB_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
+def get_db(testing: bool = False) -> Session:
+    db = SessionLocal() if not testing else TestSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 test_engine = create_engine(config.TEST_DB_URI)
 TestSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=test_engine
 )
 TestBase = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
--- tests.py
+++ tests.py
@@ -5,15 +5,34 @@ from sqlalchemy.orm import sessionmaker
 from app.main import app
 from app import db as _db
 from app import config as cfg
+from uuid import uuid4
+
+
+def _getdb(*args, **kwargs):
+    return _db.get_db(*args, testing=True, **kwargs)
+
+
+app.dependency_overrides[_db.get_db] = _getdb
+
+
+def _testuuid():
+    return str(uuid4()).replace('-', '')
+
+
+TEST_UID = _testuuid()
+TEST_ORG = 'Test Org'
+TEST_TOTP = 'JBSWY3DPEHPK3PXP'
+TEST_ENC_SEC = 'gAAAAABlq_ia8qJoDt5weWB_BKoOOrhh-FNQHwyVnV0reVIKGH74chN_PCkdWz3MR_TFOzsBqRGCvcpvHf8-f5lNZwkJxwf83_z8hBgQNoDJdiPXUj427jo='
+TEST_SEC = 'JBSWY3DPEHPK3PXP'
 
 
 # start app
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def client():
-    _db.TestBase.metadata.create_all(bind=_db.test_engine)
+    _db.Base.metadata.create_all(bind=_db.test_engine)
     with TestClient(app) as c:
         yield c
-    _db.TestBase.metadata.drop_all(bind=_db.test_engine)
+    _db.Base.metadata.drop_all(bind=_db.test_engine)
 
 
 def test_index_status(client):
@@ -26,16 +45,16 @@ def test_create_totp(client):
     response = client.post(
         "/totp",
         json={
-            "enc_secret": cfg.TEST_ENC_SEC,
-            "user_hash": cfg.TEST_UID,
-            "org_name": cfg.TEST_ORG,
+            "enc_secret": TEST_ENC_SEC,
+            "user_hash": TEST_UID,
+            "org_name": TEST_ORG,
         },
     )
     assert response.status_code == 200
     data = response.json()
-    assert data["enc_secret"] == cfg.TEST_ENC_SEC
-    assert data["user_hash"] == cfg.TEST_UID
-    assert data["org_name"] == cfg.TEST_ORG
+    assert data["enc_secret"] == TEST_ENC_SEC
+    assert data["user_hash"] == TEST_UID
+    assert data["org_name"] == TEST_ORG
 
 
 def test_read_totps(client):
@@ -46,18 +65,18 @@ def test_read_totps(client):
 
 
 def test_read_totp(client):
-    response = client.get(f"/totp/{cfg.TEST_ENC_SEC}")
+    response = client.get(f"/totp/{TEST_ENC_SEC}")
     assert response.status_code == 200
     totp = response.json()
-    assert totp["enc_secret"] == cfg.TEST_ENC_SEC
+    assert totp["enc_secret"] == TEST_ENC_SEC
 
 
 def test_delete_totp(client):
-    response = client.delete(f"/totp/{cfg.TEST_ENC_SEC}")
+    response = client.delete(f"/totp/{TEST_ENC_SEC}")
     assert response.status_code == 200
     data = response.json()
     assert data == {"status": "ok"}
 
     # Verify that the TOTP is deleted
-    response = client.get(f"/totp/{cfg.TEST_ENC_SEC}")
+    response = client.get(f"/totp/{TEST_ENC_SEC}")
     assert response.status_code == 404
